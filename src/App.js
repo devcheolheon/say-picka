6bc47a44c729cf998721db994e45de79e43a2960
cheolheonshin <cheolheonshin@gmail.com>
2021-04-08 19:09:41 +0900

    [feature] 간단한 UI 개선
    들린 단어와 피카 액션이 작동했는지 표시

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useRef, useCallback } from "react";
 import { usePikaRecognizer } from "./usePikaRecognizer.js";
 import initAndStart from "./resources/js/main.js";
 
 const App = () => {
+  const [isPika, setIsPika] = useState(false);
+  const pikatime = useRef(null);
+  const pikaHandler = useCallback(
+    (e) => {
+      if (!pikatime.current) {
+        clearTimeout(pikatime.current);
+      }
+      // 피카가 인식된후 300ms가 유지 된 후
+      // 피카가 취소됨
+      pikatime.current = setTimeout(() => {
+        setIsPika(false);
+      }, 300);
+
+      setIsPika(true);
+    },
+    [setIsPika]
+  );
+
+  useEffect(() => {
+    document.addEventListener("PIKA", pikaHandler);
+    return () => {
+      document.removeEventListener("PIKA", pikaHandler);
+    };
+  });
+
   const [
     outputText,
     startRecognizer,
@@ -23,8 +48,18 @@ const App = () => {
   }, []);
 
   return (
-    <div>
-      <div>{outputText}</div>
+    <div style={{ width: "864px" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          lineHeight: "100px",
+          fontSize: "60px",
+        }}
+      >
+        <div>👂 : {outputText}</div>
+        <div>PIKA {isPika ? <span>💡 on </span> : <span>off </span>}</div>
+      </div>
       <div id="game-canvas-container"></div>
     </div>
   );
